d3725f1ca35aa090b67dfe1f2825b5ddcb6fe4b7
Dennis Enwiya <dennisenwiya@gmail.com>
2021-03-09 16:18:12 -0700

    added more jwt, still not working

--- LoginApi/routers/user.js
+++ LoginApi/routers/user.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const User = require('../modules/users')
+const auth = require('../middleWare/auth');
+const privliges = require('../middleWare/privileges')
 const Router = new express.Router()
 
-Router.post('/register', async (req,res)=>{
-    
+Router.post('/users', async (req,res)=>{
+    const user = new User(req.body)
+  
+    try{
+        await user.save()
+        const token = user.generateAuthToken()
+        res.status(201).send({user,token})
+    }catch(e){
+        res.status(400).send(e)
+    }
 })
+module.exports = Router;
--- LoginApi/server.js
+++ LoginApi/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+require('./config/db')
+const userRouter = require('./routers/user')
 const app = express();
 const PORT = process.env.port || 9000
-require('./config/db')
+app.use(userRouter)
 const cors = require('cors')
+
 app.use(cors())
 app.listen(()=> console.log(`Server running on port ${PORT}`))
