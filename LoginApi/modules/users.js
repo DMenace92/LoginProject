472d02c896715c4fa8b714171c93ab685b186c30
Dennis Enwiya <dennisenwiya@gmail.com>
2021-03-10 23:46:56 -0700

    added username to the module schema

--- LoginApi/modules/users.js
+++ LoginApi/modules/users.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
+    username:{
+        type:String,
+        required: true,
+        trim:true
+    },
     email:{
         type: String,
         unique:true,
